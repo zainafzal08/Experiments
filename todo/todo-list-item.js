5bf0f365e3dc013a9dfaf1cffe1b158294fbdff2
Zain Afzal <zain.afz@gmail.com>
2020-02-21 22:16:38 +1100

    Fixed deletion

--- todo/todo-list-item.js
+++ todo/todo-list-item.js
@@ -33,6 +33,7 @@ class TodoListItem extends LitElement {
     this.subtitle = subtitle;
     this.checked = checked;
   }
+
   upSync() {
     tasks.updateTaskStatus(this.id, this.checked);
   }
@@ -115,6 +116,7 @@ class TodoListItem extends LitElement {
   }
 
   render() {
+    this.downSync();
     return html`
       <div class="side">
         <svg id="check" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
@@ -128,7 +130,7 @@ class TodoListItem extends LitElement {
       </div>
       <div class="delete">
         <svg id="delete" style="width:20px;height:20px" viewBox="0 0 24 24"}>
-            <path fill="var(--cross-color)" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
+          <path fill="var(--cross-color)" d="M12,2C17.53,2 22,6.47 22,12C22,17.53 17.53,22 12,22C6.47,22 2,17.53 2,12C2,6.47 6.47,2 12,2M15.59,7L12,10.59L8.41,7L7,8.41L10.59,12L7,15.59L8.41,17L12,13.41L15.59,17L17,15.59L13.41,12L17,8.41L15.59,7Z" />
         </svg>
       </div>
     `;
