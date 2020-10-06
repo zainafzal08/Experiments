5404168e3412c17fd44d572e05a3bf95fc9590bf
Zain Afzal <zain.afz@gmail.com>
2020-10-06 11:40:31 +1100

    Update index.js
    Dune got delayed :L

--- dune-countdown/index.js
+++ dune-countdown/index.js
@@ -19,7 +19,7 @@ function secondsToHuman(s) {
 }
 
 const output = document.getElementById('countdown');
-const target = moment('2020-12-13');
+const target = moment('2021-09-01');
 function tick() {
     output.innerText = secondsToHuman(target.diff(moment())/1000);
 }
