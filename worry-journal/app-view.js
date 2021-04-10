ee39634346a0739680824e490599407511a0d136
Zain Afzal <zain.afz@gmail.com>
2021-04-10 22:19:33 +1000

    Spruce up UI

--- worry-journal/app-view.js
+++ worry-journal/app-view.js
@@ -10,6 +10,9 @@ const DELETE_ICON = html`
 const RETORT_ICON = html`
 <svg xmlns='http://www.w3.org/2000/svg' fill="var(--local-color)" height="20" viewBox='0 0 512 512'><title>Chatbubbles</title><path d='M60.44 389.17c0 .07 0 .2-.08.38.03-.12.05-.25.08-.38zM439.9 405.6a26.77 26.77 0 01-9.59-2l-56.78-20.13-.42-.17a9.88 9.88 0 00-3.91-.76 10.32 10.32 0 00-3.62.66c-1.38.52-13.81 5.19-26.85 8.77-7.07 1.94-31.68 8.27-51.43 8.27-50.48 0-97.68-19.4-132.89-54.63A183.38 183.38 0 01100.3 215.1a175.9 175.9 0 014.06-37.58c8.79-40.62 32.07-77.57 65.55-104A194.76 194.76 0 01290.3 32c52.21 0 100.86 20 137 56.18 34.16 34.27 52.88 79.33 52.73 126.87a177.86 177.86 0 01-30.3 99.15l-.19.28-.74 1c-.17.23-.34.45-.5.68l-.15.27a21.63 21.63 0 00-1.08 2.09l15.74 55.94a26.42 26.42 0 011.12 7.11 24 24 0 01-24.03 24.03z'/><path d='M299.87 425.39a15.74 15.74 0 00-10.29-8.1c-5.78-1.53-12.52-1.27-17.67-1.65a201.78 201.78 0 01-128.82-58.75A199.21 199.21 0 0186.4 244.16C85 234.42 85 232 85 232a16 16 0 00-28-10.58s-7.88 8.58-11.6 17.19a162.09 162.09 0 0011 150.06C59 393 59 395 58.42 399.5c-2.73 14.11-7.51 39-10 51.91a24 24 0 008 22.92l.46.39A24.34 24.34 0 0072 480a23.42 23.42 0 009-1.79l53.51-20.65a8.05 8.05 0 015.72 0c21.07 7.84 43 12 63.78 12a176 176 0 0074.91-16.66c5.46-2.56 14-5.34 19-11.12a15 15 0 001.95-16.39z'/></svg>`;
 
+const STORM_ICON = html`
+<svg xmlns='http://www.w3.org/2000/svg' fill="var(--local-color)" height="20" viewBox='0 0 512 512'><title>Thunderstorm</title><path d='M96 416a16 16 0 01-14.3-23.16l24-48a16 16 0 0128.62 14.32l-24 48A16 16 0 0196 416zM120 480a16 16 0 01-14.3-23.16l16-32a16 16 0 0128.62 14.32l-16 32A16 16 0 01120 480zM376 416a16 16 0 01-14.3-23.16l24-48a16 16 0 0128.62 14.32l-24 48A16 16 0 01376 416zM400 480a16 16 0 01-14.3-23.16l16-32a16 16 0 0128.62 14.32l-16 32A16 16 0 01400 480z'/><path d='M405.84 136.9a151.25 151.25 0 00-47.6-81.9 153 153 0 00-241.81 51.86C60.5 110.16 16 156.65 16 213.33 16 272.15 63.91 320 122.8 320h66.31l-12.89 77.37A16 16 0 00192 416h32v64a16 16 0 0029 9.3l80-112a16 16 0 00-13-25.3h-27.51l8-32h103.84a91.56 91.56 0 001.51-183.1z'/></svg>`;
+
 class AppView extends LitElement {
     data = new Database(() => this.requestUpdate());
 
@@ -22,6 +25,8 @@ class AppView extends LitElement {
       return css`
         :host {
           width: 100vw;
+          display: flex;
+          justify-content: center;
         }
         p {
           font-size: .8rem;
@@ -50,7 +55,7 @@ class AppView extends LitElement {
           z-index: 2;
         }
         .page input {
-          width: 80%;
+          width: min(80%, 500px);
           border: none;
           font-size: 1.1rem;
           background: rgba(255, 255, 255, 0.4);
@@ -60,7 +65,6 @@ class AppView extends LitElement {
           margin: 32px 0px 16px 0px;
           text-align: center;
         }
-
         .page input:focus {
           outline: none;
         }
@@ -84,7 +88,7 @@ class AppView extends LitElement {
           z-index: 1;
         }
         .journal {
-          width: calc(100% - 64px);
+          width: 500px;
           padding: 0 32px;
           height: 100vh;
           display: flex;
@@ -94,9 +98,8 @@ class AppView extends LitElement {
         }
         .journal h1 {
           width: 100%;
-          margin-top: 64px;
+          margin-top: 32px 0;
           height: 50px;
-          margin-bottom: 64px;
           text-align: center;
           color: var(--primary-color);
           font-size: 2.4rem;
@@ -132,7 +135,7 @@ class AppView extends LitElement {
         .history {
           width: 100%;
           margin-bottom: 32px;
-          height: calc(100vh - 274px);
+          height: calc(100vh - 210px);
           overflow: scroll;
         }
 
@@ -203,6 +206,33 @@ class AppView extends LitElement {
           flex-direction: column;
           align-items: flex-end;
         }
+
+        .empty-state {
+          width: 100%;
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          --local-color: #BBB;
+        }
+        .empty-state p {
+          width: 350px;
+          margin: 0;
+          padding: 0;
+          color: #BBB;
+          text-align: center;
+        }
+        .empty-state svg {
+          width: 124px;
+          height: 124px;
+          margin-bottom: 48px;
+        }
+        .empty-state p svg {
+          width: 16px;
+          height: 16px;
+          margin-bottom: 0;
+        }
       `;
     }
   
@@ -270,7 +300,7 @@ class AppView extends LitElement {
             <path d="M53.6001 23.8668H50.6668V18.0001C50.6668 9.90411 44.0961 3.33344 36.0001 3.33344C27.9041 3.33344 21.3334 9.90411 21.3334 18.0001V23.8668H18.4001C15.1734 23.8668 12.5334 26.5068 12.5334 29.7334V59.0668C12.5334 62.2935 15.1734 64.9335 18.4001 64.9335H53.6001C56.8268 64.9335 59.4668 62.2935 59.4668 59.0668V29.7334C59.4668 26.5068 56.8268 23.8668 53.6001 23.8668ZM36.0001 50.2668C32.7735 50.2668 30.1335 47.6268 30.1335 44.4001C30.1335 41.1735 32.7735 38.5334 36.0001 38.5334C39.2268 38.5334 41.8668 41.1735 41.8668 44.4001C41.8668 47.6268 39.2268 50.2668 36.0001 50.2668ZM45.0935 23.8668H26.9068V18.0001C26.9068 12.9841 30.9841 8.90678 36.0001 8.90678C41.0161 8.90678 45.0935 12.9841 45.0935 18.0001V23.8668Z" fill="white" fill-opacity="0.4"/>
           </svg>
         </div>
-        `;
+      `;
     }
 
     renderRetort(retort) {
@@ -308,12 +338,25 @@ class AppView extends LitElement {
       </div>`;
     }
 
+    renderEmptyState() {
+      return html`
+        <div class="empty-state">
+          ${STORM_ICON}
+          <p>
+            Sometimes the mind can feel stormy. Take a breath, jot down your worry
+            or insecurity, and let it go. You can use the ${RETORT_ICON} button later
+            to rebutt your worry.
+          </p>
+        </div>`;
+    }
+
     renderJournal() {
       return html`
       <div class="journal" @keydown=${(e) => e.key === 'Enter' && this.addItem()}>
         <h1>Your Worries</h1>
         <div class="history">
           ${this.data.items.map(item => this.renderItem(item))}
+          ${this.data.items.length === 0 ? this.renderEmptyState() : ''}
         </div>
         <div class="new-entry">
         <input id="new-item"/>
