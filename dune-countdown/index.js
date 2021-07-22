12288eaf8b8235ef0a32bbc2fcf786cb5c3c7346
Zain Afzal <zain.afz@gmail.com>
2021-07-22 22:12:14 +1000

    This mf keep getting delayed

--- dune-countdown/index.js
+++ dune-countdown/index.js
@@ -19,7 +19,7 @@ function secondsToHuman(s) {
 }
 
 const output = document.getElementById('countdown');
-const target = moment('2021-09-01');
+const target = moment('2021-10-22');
 function tick() {
     output.innerText = secondsToHuman(target.diff(moment())/1000);
 }
